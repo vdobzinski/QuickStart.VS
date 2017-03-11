634e74307ed467616eea5c4ae99b692e7617cdf4
Vladislav Dobžinski <vdobzinski@gmail.com>
2017-03-11 23:11:39 +0200

    Add project files.

---
+++ QuickStart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuickStart.MvcApplication" Language="C#" %>
---
+++ QuickStart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuickStart.Startup))]
+namespace QuickStart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
